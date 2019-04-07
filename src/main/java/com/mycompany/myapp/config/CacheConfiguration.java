a0440910a255bf7bcccac14252a11f41520ed2e2
linyc <582847391@qq.com>
2019-04-07 11:54:25 +0800

    通过jhipster import-jdl ./jhipster-jdl.jh  生成实体

--- src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -1,16 +1,17 @@
 package com.mycompany.myapp.config;
 
-import java.time.Duration;
-
-import org.ehcache.config.builders.*;
-import org.ehcache.jsr107.Eh107Configuration;
-
-import io.github.jhipster.config.jcache.BeanClassLoaderAwareJCacheRegionFactory;
 import io.github.jhipster.config.JHipsterProperties;
-
+import io.github.jhipster.config.jcache.BeanClassLoaderAwareJCacheRegionFactory;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.jsr107.Eh107Configuration;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -38,6 +39,18 @@ public class CacheConfiguration {
             cm.createCache(com.mycompany.myapp.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.mycompany.myapp.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.mycompany.myapp.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Region.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Country.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Location.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Department.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Department.class.getName() + ".employees", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Task.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Task.class.getName() + ".jobs", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Employee.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Employee.class.getName() + ".jobs", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Job.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Job.class.getName() + ".tasks", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.JobHistory.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'region',
+                loadChildren: './region/region.module#App1RegionModule'
+            },
+            {
+                path: 'country',
+                loadChildren: './country/country.module#App1CountryModule'
+            },
+            {
+                path: 'location',
+                loadChildren: './location/location.module#App1LocationModule'
+            },
+            {
+                path: 'department',
+                loadChildren: './department/department.module#App1DepartmentModule'
+            },
+            {
+                path: 'task',
+                loadChildren: './task/task.module#App1TaskModule'
+            },
+            {
+                path: 'employee',
+                loadChildren: './employee/employee.module#App1EmployeeModule'
+            },
+            {
+                path: 'job',
+                loadChildren: './job/job.module#App1JobModule'
+            },
+            {
+                path: 'job-history',
+                loadChildren: './job-history/job-history.module#App1JobHistoryModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
